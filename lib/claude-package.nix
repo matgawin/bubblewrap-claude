93c7d98de679d528b4037d3ba5283a71c23a4941
matgawin <8750553+matgawin@users.noreply.github.com>
2025-10-25 01:02:07 +0000

    chore: Update claude-code to v2.0.27

--- lib/claude-package.nix
+++ lib/claude-package.nix
@@ -1,8 +1,8 @@
 {pkgs}: let
-  version = "2.0.26";
+  version = "2.0.27";
   claudeCodeTarball = pkgs.fetchurl {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-Bh087VnFRrp5agxcq+4gzSGS1rhYZKnXra1kSfJGyWs=";
+    hash = "sha256-2zKiK5RIMGHxRa4OsBgq5bYdJLW3DFlZQF8xtYgNsTc=";
   };
 in
   pkgs.stdenv.mkDerivation {
