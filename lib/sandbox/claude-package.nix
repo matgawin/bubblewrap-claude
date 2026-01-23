0fb965b0e573ca3b625cc424c32211445ef479cf
matgawin <8750553+matgawin@users.noreply.github.com>
2026-01-23 01:13:59 +0000

    chore: Update claude-code to v2.1.17

--- lib/sandbox/claude-package.nix
+++ lib/sandbox/claude-package.nix
@@ -1,8 +1,8 @@
 {pkgs}: let
-  version = "2.1.15";
+  version = "2.1.17";
   claudeCodeTarball = pkgs.fetchurl {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-6B3q0Z2AXqD8UCvuCjyx90LxYrjZDgfa7y6htBicqL4=";
+    hash = "sha256-Tug122dgT/TpTeEpBQx38O948CxLf59VOfsDF+Dde6w=";
   };
 in
   pkgs.stdenv.mkDerivation {
